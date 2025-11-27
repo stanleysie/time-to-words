b6e8a63f25c905261b389be378d784b9a2a7dc9b
stanleysie <stanleysie.dev@gmail.com>
2025-11-27 15:14:28 +0800

    Add solution for pair programming test

--- index.js
+++ index.js
@@ -4,8 +4,61 @@ function convertTimeToWords(time) {
   if (time === '0:00') {
     return 'midnight';
   }
+  if (time === '12:00') {
+    return 'midday';
+  }
 
-  return 'half past eight';
+  const DIGIT_MAPPING = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'quarter',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    21: 'twenty one',
+    22: 'twenty two',
+    23: 'twenty three',
+    24: 'twenty four',
+    25: 'twenty five',
+    26: 'twenty six',
+    27: 'twenty seven',
+    28: 'twenty eight',
+    29: 'twenty nine',
+    30: 'half',
+  };
+
+  let [hour, minute] = time.split(':');
+  hour = Number(hour);
+  minute = Number(minute);
+
+  if (minute === 0) {
+    return `${DIGIT_MAPPING[hour]} o'clock`;
+  }
+
+  if (minute <= 30) {
+    return `${DIGIT_MAPPING[minute]} past ${DIGIT_MAPPING[hour]}`;
+  }
+
+  if (minute > 30) {
+    hour = hour === 12 ? 1 : hour + 1;
+    return `${DIGIT_MAPPING[60 - minute]} to ${DIGIT_MAPPING[hour]}`;
+  }
+
+  return 'Invalid time';
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,9 +1,14 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require('./index');
 
 describe('Time to words', () => {
   it('Handles midnight', () => {
     const timeInWords = convertTimeToWords('0:00');
     expect(timeInWords).toBe('midnight');
+  });
+
+  it('Handles midday', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
   });
 
   it('Handles 30 - 8:30', () => {
@@ -14,5 +19,30 @@ describe('Time to words', () => {
   it('Handles times after 30 mins - 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
+  });
+
+  it('Handles times after 30 mins - 2:43', () => {
+    const timeInWords = convertTimeToWords('2:43');
+    expect(timeInWords).toBe('seventeen to three');
+  });
+
+  it('Handles times before 30 mins - 1:15', () => {
+    const timeInWords = convertTimeToWords('1:15');
+    expect(timeInWords).toBe('quarter past one');
+  });
+
+  it('Handles times before 30 mins - 1:10', () => {
+    const timeInWords = convertTimeToWords('1:10');
+    expect(timeInWords).toBe('ten past one');
+  });
+
+  it('Handles times from 12 to 1', () => {
+    const timeInWords = convertTimeToWords('12:48');
+    expect(timeInWords).toBe('twelve to one');
+  });
+
+  it("Handles times with o'clock", () => {
+    const timeInWords = convertTimeToWords('8:00');
+    expect(timeInWords).toBe("eight o'clock");
   });
 });
